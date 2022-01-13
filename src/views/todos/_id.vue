9e57c69b3543eeafc78f2354b3af6325a6266d34
junhochoi <braverokmc79@gmail.com>
2022-01-13 23:32:08 +0900

    49. Todo 페이지컴포넌트 만들기2

--- src/views/todos/_id.vue
+++ src/views/todos/_id.vue
@@ -3,23 +3,49 @@
     <h1>To-Do Page</h1>
     <div v-if="loading">loading...</div>
     <form v-else>
-      <div class="form-group">
-        <label>제목</label>
-        <input type="text" class="form-control" v-model="todo.subject" />
+      <div class="row">
+        <div class="col-6">
+          <div class="form-group">
+            <label>제목</label>
+            <input type="text" class="form-control" v-model="todo.subject" />
+          </div>
+        </div>
+
+        <div class="col-6">
+          <div class="form-group">
+            <label>상태</label>
+            <div>
+              <button
+                type="button"
+                class="btn"
+                :class="todo.completed ? 'btn-success' : 'btn-danger'"
+                @click.stop="toggleTodoStatus"
+              >
+                {{ todo.completed ? "Completed" : "Incompleted" }}
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <button class="btn btn-dark">저장</button>
+
+      <button type="submit" class="btn btn-info">저장</button>
+
+      <button type="button" class="btn btn-dark ml-2" @click="moveTodoListpage">
+        취소
+      </button>
     </form>
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import axios from "axios";
 
 export default {
   setup() {
     const route = useRoute();
+    const router = useRouter();
     const todo = ref(null);
     const loading = ref(true);
 
@@ -33,9 +59,31 @@ export default {
 
     getTodo();
 
+    const toggleTodoStatus = () => {
+      // const res = await axios.patch("http://localhost:3000/todos/" + id, {
+      //   completed: !completed,
+      // });
+      // console.log(res);
+      // router.push({
+      //   name: "Todo",
+      //   params: {
+      //     id: id,
+      //   },
+      // });
+      todo.value.completed = !todo.value.completed;
+    };
+
+    const moveTodoListpage = () => {
+      router.push({
+        name: "Todos",
+      });
+    };
+
     return {
       todo,
       loading,
+      toggleTodoStatus,
+      moveTodoListpage,
     };
   },
 };
